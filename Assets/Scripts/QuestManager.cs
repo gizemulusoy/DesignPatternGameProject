9d2c1a88ae060874a838e2b3f4b9aa60b42a658d
gizemulusoy <gizemulusoy2002@hotmail.com>
2025-04-25 20:03:39 +0300

    Some sprites added, dialogue box added.

---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using TMPro;
+
+public class QuestManager : MonoBehaviour
+{
+    public Transform leyla;              // Leyla karakteri
+    public Transform mudur;              // Müdür karakteri
+    public TMP_Text questText;           // TextMeshPro Text objesi
+    public float mesafeLimit = 3f;       // Ne kadar yaklaşınca yazı değişecek?
+
+    private bool mudureYaklasti = false;
+
+    void Start()
+    {
+        questText.text = "Hadi görevi öğrenmek için müdürün yanına git";
+    }
+
+    void Update()
+    {
+        float mesafe = Vector2.Distance(leyla.position, mudur.position);
+
+        if (!mudureYaklasti && mesafe <= mesafeLimit)
+        {
+            mudureYaklasti = true;
+            questText.text = "hasNext() Leyla depoda getirecek makarna var mı kontrol et";
+        }
+    }
+}
